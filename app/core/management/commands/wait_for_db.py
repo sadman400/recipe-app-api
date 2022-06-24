e24d74b34337dbdc289084048c4e7896b3c82763
sadman400 <sadmanhossain400@gmail.com>
2022-06-25 02:41:59 +0600

    Configured docker compose and checks to wait for db

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -13,7 +13,6 @@ class Command(BaseCommand):
     """Django command to wait for database."""
 
     def handle(self, *args, **options):
-        pass
         """Entrypoint for command."""
         self.stdout.write('Waiting for database...')
         db_up = False
