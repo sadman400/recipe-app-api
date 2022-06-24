321b26d5553bfc4ceffab315dfc1f69f3c7def7d
sadman400 <sadmanhossain400@gmail.com>
2022-06-25 03:29:20 +0600

    Configured docker compose and checks to wait for db Checks

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,3 +1,4 @@
+
 """
 Sample tests
 """
@@ -7,10 +8,6 @@ from app import calc
 
 
 class CalcTests(SimpleTestCase):
-    """Test the calc module."""
-
     def test_add_numbers(self):
-        """Test adding numbers together."""
         res = calc.add(5, 6)
-
         self.assertEqual(res, 11)
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,7 +1,6 @@
 """
 Test custom Django management commands.
 """
-
 from unittest.mock import patch
 
 from psycopg2 import OperationalError as Psycopg2OpError
@@ -9,6 +8,7 @@ from psycopg2 import OperationalError as Psycopg2OpError
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
+
 
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
